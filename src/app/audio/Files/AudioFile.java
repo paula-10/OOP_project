108de8bd40579745986139f63928613a430fc213
Paula Rusen <paula.rusen03@gmail.com>
2024-01-16 20:12:32 +0200

    Downloaded checker

---
+++ src/app/audio/Files/AudioFile.java
@@ -0,0 +1,18 @@
+package app.audio.Files;
+
+import app.audio.LibraryEntry;
+import lombok.Getter;
+
+@Getter
+public abstract class AudioFile extends LibraryEntry {
+    private final Integer duration;
+
+    public AudioFile(final String name, final Integer duration) {
+        super(name);
+        this.duration = duration;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+}
